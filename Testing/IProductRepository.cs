63162c7ac953813a394c305815ba96d25cdce15d
Matthew Ferguson <you@example.com>
2022-02-15 09:56:42 -0600

    done

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+
+namespace Testing.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+
+        public void UpdateProduct (Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
+
+
+    }
+}
